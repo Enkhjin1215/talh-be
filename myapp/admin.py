33d4c4ce627e2fe992750023350ba21e2e4facb0
Enkhjin Ganaa <49260986+Enkhjin1215@users.noreply.github.com>
2026-01-06 14:35:23 +0800

    changes

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,42 @@
 from django.contrib import admin
 from .models import Lottery
 
+import pandas as pd
+from django.http import HttpResponse
+from django.utils.timezone import localtime
+
+
+def export_lottery_to_excel(modeladmin, request, queryset):
+    data = []
+
+    for obj in queryset:
+        data.append({
+            'Утас': obj.utas,
+            'И-мэйл': obj.email,
+            'Бүтэн нэр': obj.buten_ner,
+            'Хотын нэр': obj.hotiin_ner,
+            'Сумын нэр': obj.sumiin_ner,
+            'Багийн нэр': obj.bagiin_ner,
+            'Захидал': obj.letter,
+            'Статус': obj.status,
+            'Бүртгэгдсэн огноо': localtime(obj.created_at).replace(tzinfo=None),
+            'Е-баримтын зураг': obj.ebarimt_picture.url if obj.ebarimt_picture else '',
+        })
+
+    df = pd.DataFrame(data)
+
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = 'attachment; filename=lottery.xlsx'
+
+    df.to_excel(response, index=False)
+    return response
+
+
+
+
+
 
 @admin.register(Lottery)
 class LotteryAdmin(admin.ModelAdmin):
@@ -52,3 +88,4 @@ class LotteryAdmin(admin.ModelAdmin):
             )
         }),
     )
+    actions = [export_lottery_to_excel]
